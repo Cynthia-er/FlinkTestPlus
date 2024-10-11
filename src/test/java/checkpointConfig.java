b0c0125b15b9550812efbc2046eac6fa209ea34c
worker-21 <2574525350@qq.com>
2024-10-11 11:23:25 +0800

    first commit

--- src/test/java/checkpointConfig.java
+++ src/test/java/checkpointConfig.java
@@ -1,18 +1,30 @@
+import org.apache.flink.contrib.streaming.state.RocksDBStateBackend;
+import org.apache.flink.runtime.state.StateBackend;
 import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class checkpointConfig{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        // 设置状态后端（例如使用 RocksDB）
+        /**
+         * MemoryStateBackend（默认）：状态存储在内存中，仅适合小状态的情况。
+         * FsStateBackend：状态存储在文件系统中，适合较大的状态，但需要额外的存储配置。
+         * RocksDBStateBackend：基于 RocksDB 的状态后端，适合超大状态的处理，并支持持久化到本地文件系统或 HDFS。
+         */
+        StateBackend stateBackend = new RocksDBStateBackend("hdfs://hadoop102:8020/ckpt");
+        env.setStateBackend(stateBackend);
+
         env.enableCheckpointing( 2000, CheckpointingMode.EXACTLY_ONCE);  // 传入两个最基本ck参数；间隔时长，ck模式
         CheckpointConfig checkpointConfig =env.getCheckpointConfig();
-        checkpointConfig.setCheckpointStorage("hdfs://doit01:8020/ckpt");
+        checkpointConfig.setCheckpointStorage("hdfs://hadoop102:8020/ckpt");
         checkpointConfig.setAlignedCheckpointTimeout(Duration.ofMinutes(10000)); // 设置ck对齐的超时时长
         checkpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE); // 设置ck算法模式
         checkpointConfig.setCheckpointInterval(2000); // ck的间隔时长
